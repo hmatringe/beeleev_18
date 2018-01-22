101d95f3c1105d844b14c0108ab7f75cd90de412
Matringe <hadrien.matringe@gmail.com>
2018-01-22 11:27:06 +0100

    banner correct according to sketch

--- app/assets/javascripts/navbar-change.js
+++ app/assets/javascripts/navbar-change.js
@@ -1,8 +1,22 @@
+
 $(window).scroll(function() {    
+
+
+    var navbar_switch = document.getElementById('navbar-switch');
+
+    window.onscroll = function() {
+      checkVisible(navbar_switch) ? console.log('visible') : console.log('hidden');
+    };
+
+    function checkVisible(elm) {
+      var rect = elm.getBoundingClientRect();
+      var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+      return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+    }
 
     var scroll = $(window).scrollTop();
 
-    if (scroll > 505 && scroll < 2005 || scroll > 2677) {
+    if (scroll > 505 && scroll < 2005 || window.onscroll = function() {checkVisible(navbar_switch)};) {
         $('.lemenu18_transparent').addClass('scrolled_transparent')
         $('#white-logo').addClass('logo-hidden')
         $('#blue-logo').removeClass('logo-hidden')
